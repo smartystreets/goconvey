639d7d0345a9d1c6b2817130babf7a7ef22c670c
Michael Whatcott <mdwhatcott@gmail.com>
2013-09-04 13:13:45 -0600

    Implemented ShouldNotEqual expectation.

--- convey/expectations.go
+++ convey/expectations.go
@@ -1,6 +1,9 @@
 package convey
 
-import "fmt"
+import (
+	"fmt"
+	_ "reflect"
+)
 
 type expectation func(actual interface{}, expected ...interface{}) string
 
@@ -9,6 +12,15 @@ func ShouldEqual(actual interface{}, expected ...interface{}) string {
 		return fail
 	} else if actual != expected[0] {
 		return fmt.Sprintf(shouldHaveBeenEqual, actual, expected[0])
+	}
+	return success
+}
+
+func ShouldNotEqual(actual interface{}, expected ...interface{}) string {
+	if fail := onlyOne(expected); fail != "" {
+		return fail
+	} else if actual == expected[0] {
+		return fmt.Sprintf(shouldNotHaveBeenEqual, actual, expected[0])
 	}
 	return success
 }
@@ -24,51 +36,52 @@ func ShouldBeNil(actual interface{}, expected ...interface{}) string {
 
 /*
 
-	// Equality
-X	So(thing, ShouldEqual, thing2)
-	So(thing, ShouldNotEqual, thing2)
-	So(thing, ShouldMarshalLike, thing2) // not necessary if we use DeepEquals for ShouldEqual and ShouldNotEqual?
-	So(thing, ShouldPointTo, thing2)
-	So(thing, ShouldNotPointTo, thing2)
-X	So(thing, ShouldBeNil, thing2)
-	So(thing, ShouldNotBeNil, thing2)
-	So(thing, ShouldBeTrue)
-	SO(thing, ShouldBeFalse)
+1.0		// Equality
+*	X	So(thing, ShouldEqual, thing2)
+*	X	So(thing, ShouldNotEqual, thing2)
+*		So(thing, ShouldBeLike, thing2)
+*		So(thing, ShouldNotBeLike, thing2)
+		So(thing, ShouldPointTo, thing2)
+		So(thing, ShouldNotPointTo, thing2)
+*	X	So(thing, ShouldBeNil, thing2)
+*		So(thing, ShouldNotBeNil, thing2)
+*		So(thing, ShouldBeTrue)
+*		SO(thing, ShouldBeFalse)
 
-	// Interfaces
-	So(1, ShouldImplement, Interface)
-	So(1, ShouldNotImplement, OtherInterface)
+		// Interfaces
+		So(1, ShouldImplement, Interface)
+		So(1, ShouldNotImplement, OtherInterface)
 
-	// Type checking
-	So(1, ShouldBeAn, int)
-	So(1, ShouldNotBeAn, int)
-	So("1", ShouldBeA, string)
-	So("1", ShouldNotBeA, string)
+		// Type checking
+		So(1, ShouldBeAn, int)
+		So(1, ShouldNotBeAn, int)
+		So("1", ShouldBeA, string)
+		So("1", ShouldNotBeA, string)
 
-	// Quantity comparison
-	So(1, ShouldBeGreaterThan, 0)
-	So(1, ShouldBeGreaterThanOrEqualTo, 0)
-	So(1, ShouldBeLessThan, 2)
-	So(1, ShouldBeLessThanOrEqualTo, 2)
+		// Quantity comparison (Time and Duration might be special cases?)
+*		So(1, ShouldBeGreaterThan, 0)
+*		So(1, ShouldBeGreaterThanOrEqualTo, 0)
+*		So(1, ShouldBeLessThan, 2)
+*		So(1, ShouldBeLessThanOrEqualTo, 2)
 
-	// Tolerences
-	So(1.1, ShouldBeWithin(.1).Of, 1)
-	So(1.1, ShouldNotBeWithin(.1).Of, 2)
+		// Tolerences
+*		So(1.1, ShouldBeWithin, .1, 1)
+*		So(1.1, ShouldNotBeWithin, .1, 2)
 
-	// Collections
-	So([]int{}, ShouldBeEmpty)
-	So([]int{1}, ShouldNotBeEmpty)
-	So([]int{1, 2, 3}, ShouldContain, 1) // This could receive several final arguments as proposed members
-	So([]int{1, 2, 3}, ShouldNotContain, 4) // This could receive several final arguments as proposed members
-	So(1, ShouldBeIn, []int{1, 2, 3})
-	So(4, ShouldNotBeIn, []int{1, 2, 3})
+		// Collections
+*		So([]int{}, ShouldBeEmpty)
+*		So([]int{1}, ShouldNotBeEmpty)
+*		So([]int{1, 2, 3}, ShouldContain, 1, 2) // This could receive several final arguments as proposed members
+*		So([]int{1, 2, 3}, ShouldNotContain, 4, 5) // This could receive several final arguments as proposed members
+		So(1, ShouldBeIn, []int{1, 2, 3})
+		So(4, ShouldNotBeIn, []int{1, 2, 3})
 
-	// Strings
-	So("asdf", ShouldStartWith, "as")
-	So("asdf", ShouldNotStartWith, "df")
-	So("asdf", ShouldEndWith, "df")
-	So("asdf", ShouldNotEndWith, "df")
-	So("(asdf)", ShouldBeSurroundedWith, "()")
-	So("(asdf)", ShouldNotBeSurroundedWith, "[]")
+		// Strings
+*		So("asdf", ShouldStartWith, "as")
+*		So("asdf", ShouldNotStartWith, "df")
+*		So("asdf", ShouldEndWith, "df")
+*		So("asdf", ShouldNotEndWith, "df")
+		So("(asdf)", ShouldBeSurroundedWith, "()")
+		So("(asdf)", ShouldNotBeSurroundedWith, "[]")
 
 */
--- convey/expectations_test.go
+++ convey/expectations_test.go
@@ -1,6 +1,11 @@
 package convey
 
-import "testing"
+import (
+	_ "fmt"
+	_ "reflect"
+	"runtime"
+	"testing"
+)
 
 func TestShouldEqual(t *testing.T) {
 	fail(t, so(1, ShouldEqual), "This expectation requires at least one comparison value (none provided).")
@@ -16,7 +21,30 @@ func TestShouldEqual(t *testing.T) {
 	pass(t, so("hi", ShouldEqual, "hi"))
 	fail(t, so("hi", ShouldEqual, "bye"), "'hi' should equal 'bye' (but it doesn't)!")
 
-	// TODO: compare structs
+	pass(t, so(thing1{}, ShouldEqual, thing1{}))
+	fail(t, so(&thing1{"hi"}, ShouldEqual, &thing1{"hi"}), "'&{hi}' should equal '&{hi}' (but it doesn't)!")
+
+	fail(t, so(thing1{}, ShouldEqual, thing2{}), "'{}' should equal '{}' (but it doesn't)!")
+}
+
+func TestShouldNotEqual(t *testing.T) {
+	fail(t, so(1, ShouldNotEqual), "This expectation requires at least one comparison value (none provided).")
+	fail(t, so(1, ShouldNotEqual, 1, 2), "This expectation only accepts 1 value to be compared (and 2 were provided).")
+	fail(t, so(1, ShouldNotEqual, 1, 2, 3), "This expectation only accepts 1 value to be compared (and 3 were provided).")
+
+	pass(t, so(1, ShouldNotEqual, 2))
+	fail(t, so(1, ShouldNotEqual, 1), "'1' should NOT equal '1' (but it does)!")
+
+	pass(t, so(true, ShouldNotEqual, false))
+	fail(t, so(true, ShouldNotEqual, true), "'true' should NOT equal 'true' (but it does)!")
+
+	pass(t, so("hi", ShouldNotEqual, "bye"))
+	fail(t, so("hi", ShouldNotEqual, "hi"), "'hi' should NOT equal 'hi' (but it does)!")
+
+	pass(t, so(&thing1{"hi"}, ShouldNotEqual, &thing1{"hi"}))
+	fail(t, so(thing1{}, ShouldNotEqual, thing1{}), "'{}' should NOT equal '{}' (but it does)!")
+
+	pass(t, so(thing1{}, ShouldNotEqual, thing2{}))
 }
 
 func TestShouldBeNil(t *testing.T) {
@@ -30,7 +58,8 @@ func TestShouldBeNil(t *testing.T) {
 func pass(t *testing.T, result string) {
 	const PASS = success
 	if result != PASS {
-		t.Errorf("Expectation should have passed but failed: '%s'", result)
+		_, _, line, _ := runtime.Caller(1)
+		t.Errorf("Expectation should have passed but failed (see line %d): '%s'", line, result)
 	}
 }
 
@@ -39,11 +68,19 @@ func fail(t *testing.T, actual string, expected string) {
 		if actual == "" {
 			actual = "(empty)"
 		}
-		t.Errorf("Expectation should have failed. \nExpected: %s\nActual:   %s\n",
-			expected, actual)
+		_, _, line, _ := runtime.Caller(1)
+		t.Errorf("Expectation should have failed but passed (see line %d). \nExpected: %s\nActual:   %s\n",
+			line, expected, actual)
 	}
 }
 
 func so(actual interface{}, match expectation, expected ...interface{}) string {
 	return match(actual, expected...)
+}
+
+type thing1 struct {
+	a string
+}
+type thing2 struct {
+	a string
 }
