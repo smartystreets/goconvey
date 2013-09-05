aa8bd5c3d15696a1081fd46a32b715100d6820ca
Michael Whatcott <mdwhatcott@gmail.com>
2013-09-05 11:43:47 -0600

    Typos in the docs.

--- examples/doc.go
+++ examples/doc.go
@@ -1,5 +1,5 @@
 // Package examples contains, well, examples of how to use goconvey to
-// specify behavior or a system under test. There is a well-known example
+// specify behavior of a system under test. It contains a well-known example
 // by Robert C. Martin called "Bowling Game Kata" as well as another very
 // trivial example that demonstrates Reset() and some of the assertions.
 package examples
