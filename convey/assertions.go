3a6dc655e64fa26fb0191f5b5c918426b144341d
Michael Whatcott <mdwhatcott@gmail.com>
2013-09-18 17:02:40 -0600

    Wired up all the assertions we support.

--- convey/assertions.go
+++ convey/assertions.go
@@ -4,11 +4,15 @@ import (
 	"github.com/smartystreets/goconvey/assertions"
 )
 
+// TODO: make examples file that invokes each of these so we know it's all working.
+
 var (
 	ShouldEqual       = assertions.ShouldEqual
 	ShouldNotEqual    = assertions.ShouldNotEqual
 	ShouldResemble    = assertions.ShouldResemble
 	ShouldNotResemble = assertions.ShouldNotResemble
+	ShouldPointTo     = assertions.ShouldPointTo
+	ShouldNotPointTo  = assertions.ShouldNotPointTo
 	ShouldBeNil       = assertions.ShouldBeNil
 	ShouldNotBeNil    = assertions.ShouldNotBeNil
 	ShouldBeTrue      = assertions.ShouldBeTrue
@@ -19,4 +23,35 @@ var (
 	ShouldBeLessThan             = assertions.ShouldBeLessThan
 	ShouldBeLessThanOrEqualTo    = assertions.ShouldBeLessThanOrEqualTo
 	ShouldBeBetween              = assertions.ShouldBeBetween
+	ShouldNotBeBetween           = assertions.ShouldNotBeBetween
+
+	ShouldContain    = assertions.ShouldContain
+	ShouldNotContain = assertions.ShouldNotContain
+	ShouldBeIn       = assertions.ShouldBeIn
+	ShouldNotBeIn    = assertions.ShouldNotBeIn
+
+	ShouldStartWith    = assertions.ShouldStartWith
+	ShouldNotStartWith = assertions.ShouldNotStartWith
+	ShouldEndWith      = assertions.ShouldEndWith
+	ShouldNotEndWith   = assertions.ShouldNotEndWith
+	ShouldBeBlank      = assertions.ShouldBeBlank
+	ShouldNotBeBlank   = assertions.ShouldNotBeBlank
+
+	ShouldPanic        = assertions.ShouldPanic
+	ShouldNotPanic     = assertions.ShouldNotPanic
+	ShouldPanicWith    = assertions.ShouldPanicWith
+	ShouldNotPanicWith = assertions.ShouldNotPanicWith
+
+	ShouldHaveSameTypeAs    = assertions.ShouldHaveSameTypeAs
+	ShouldNotHaveSameTypeAs = assertions.ShouldNotHaveSameTypeAs
+
+	ShouldHappenBefore         = assertions.ShouldHappenBefore
+	ShouldHappenOnOrBefore     = assertions.ShouldHappenOnOrBefore
+	ShouldHappenAfter          = assertions.ShouldHappenAfter
+	ShouldHappenOnOrAfter      = assertions.ShouldHappenOnOrAfter
+	ShouldHappenBetween        = assertions.ShouldHappenBetween
+	ShouldHappenOnOrBetween    = assertions.ShouldHappenOnOrBetween
+	ShouldNotHappenOnOrBetween = assertions.ShouldNotHappenOnOrBetween
+	ShouldHappenWithin         = assertions.ShouldHappenWithin
+	ShouldNotHappenWithin      = assertions.ShouldNotHappenWithin
 )
