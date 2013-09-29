fab61a328989501462b7b77c086f621d63e72e25
Michael Whatcott <mdwhatcott@gmail.com>
2013-09-28 21:30:38 -0600

    Retooled the json reporter. Scopes are in an array with a depth field, not a nested, recursive structure.
    It will be much easier to template this kind of json structure into
    HTML.

--- reporting/json.go
+++ reporting/json.go
@@ -8,28 +8,33 @@ import (
 )
 
 func (self *jsonReporter) BeginStory(test gotest.T) {
-	self.story = newScope("TOP")
-	self.current = self.story
+	top := newScope("TOP", self.depth) // TODO: this could have the GoTest name.
+	self.scopes = append(self.scopes, top)
+	self.stack = append(self.stack, top)
 }
 
 func (self *jsonReporter) Enter(title, id string) {
-	if _, found := self.titlesById[id]; found {
-		return
+	self.depth++
+	if _, found := self.titlesById[id]; !found {
+		self.registerScope(title, id)
 	}
-
+}
+func (self *jsonReporter) registerScope(title, id string) {
 	self.titlesById[id] = title
-	child := newScope(title)
-	self.current.Children = append(self.current.Children, child)
-	self.current = child
+	next := newScope(title, self.depth)
+	self.scopes = append(self.scopes, next)
+	self.stack = append(self.stack, next)
 }
 
 func (self *jsonReporter) Report(r *Report) {
-	self.current.Reports = append(self.current.Reports, r)
+	current := self.stack[len(self.stack)-1]
+	current.Reports = append(current.Reports, newJsonReport(r))
 }
 
 func (self *jsonReporter) Exit() {
-	if self.current.parent != nil {
-		self.current = self.current.parent
+	self.depth--
+	if len(self.stack) > 0 {
+		self.stack = self.stack[:len(self.stack)-1]
 	}
 }
 
@@ -38,42 +43,65 @@ func (self *jsonReporter) EndStory() {
 	self.reset()
 }
 func (self *jsonReporter) report() {
-	serialized, _ := json.Marshal(self.story)
+	serialized, _ := json.Marshal(self.scopes)
 	var buffer bytes.Buffer
-	json.Indent(&buffer, serialized, "", " ")
-	self.out.Print(buffer.String())
+	json.Indent(&buffer, serialized, "", "  ")
+	self.out.Print(buffer.String() + ",")
 }
 func (self *jsonReporter) reset() {
 	self.titlesById = make(map[string]string)
-	self.story = nil
-	self.current = nil
+	self.scopes = []*scope{}
+	self.stack = []*scope{}
+	self.depth = 0
 }
 
 func NewJsonReporter(out *printing.Printer) *jsonReporter {
 	self := &jsonReporter{}
-	self.titlesById = make(map[string]string)
 	self.out = out
+	self.reset()
 	return self
 }
 
 type jsonReporter struct {
 	out        *printing.Printer
-	story      *scope
-	current    *scope
 	titlesById map[string]string
+	scopes     []*scope
+	stack      []*scope
+	depth      int
 }
 
 type scope struct {
-	Title    string
-	Reports  []*Report
-	Children []*scope
-	parent   *scope
+	Title   string
+	File    string
+	Line    int
+	Depth   int
+	Reports []*report
 }
 
-func newScope(title string) *scope {
+func newScope(title string, depth int) *scope {
 	self := &scope{}
+	self.Depth = depth
 	self.Title = title
-	self.Reports = []*Report{}
-	self.Children = []*scope{}
+	self.Reports = []*report{}
 	return self
+}
+
+type report struct {
+	File       string
+	Line       int
+	Failure    string
+	Error      interface{}
+	StackTrace string
+	Skipped    bool
+}
+
+func newJsonReport(r *Report) *report {
+	return &report{
+		r.File,
+		r.Line,
+		r.Failure,
+		r.Error,
+		r.stackTrace,
+		r.Skipped,
+	}
 }
