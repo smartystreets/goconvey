ce9debe081f7a6f98131b7d50e67ce8ba3b429db
Michael Whatcott <mdwhatcott@gmail.com>
2013-09-30 17:08:04 -0600

    Included a few TODO items.

--- gotest/utils.go
+++ gotest/utils.go
@@ -11,6 +11,7 @@ func ResolveExternalFunctionName() string {
 	return name
 }
 
+// TODO: rethink this method...
 func ResolveExternalFileAndLine() string {
 	file, line, _ := ResolveExternalCaller()
 	if line == -1 {
@@ -19,6 +20,7 @@ func ResolveExternalFileAndLine() string {
 	return fmt.Sprintf("%s:%d", file, line)
 }
 
+// TODO: rename to ResolveFirstExternalCaller()
 func ResolveExternalCaller() (file string, line int, name string) {
 	var caller_id uintptr
 	callers := runtime.Callers(0, callStack)
