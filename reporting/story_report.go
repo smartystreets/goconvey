dad15db1b436faebb280cd876eac1ca0964e7840
Michael Whatcott <mdwhatcott@gmail.com>
2013-09-30 22:40:44 -0600

    Just use concise test function name.

--- reporting/story_report.go
+++ reporting/story_report.go
@@ -1,7 +1,9 @@
 package reporting
 
 import (
+	_ "fmt"
 	"github.com/smartystreets/goconvey/gotest"
+	"strings"
 )
 
 type StoryReport struct {
@@ -13,10 +15,21 @@ type StoryReport struct {
 
 func NewStoryReport(test gotest.T) *StoryReport {
 	file, line, name := gotest.ResolveExternalCaller()
+	name = removePackagePath(name)
 	self := &StoryReport{}
 	self.Test = test
 	self.Name = name
 	self.File = file
 	self.Line = line
 	return self
+}
+
+// name comes in looking like "github.com/smartystreets/goconvey/examples.TestName".
+// We only want the stuff after the last '.', which is the name of the test function.
+func removePackagePath(name string) string {
+	parts := strings.Split(name, ".")
+	if len(parts) == 1 {
+		return name
+	}
+	return parts[len(parts)-1]
 }
