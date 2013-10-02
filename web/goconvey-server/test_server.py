707ad5f15046dc115e05778f2aa87ea95ada458e
Michael Whatcott <mdwhatcott@gmail.com>
2013-10-02 17:10:52 -0600

    Stubbed out a python script to test the web server.

---
+++ web/goconvey-server/test_server.py
@@ -0,0 +1,17 @@
+"""
+This integration test script will execute various HTTP actions against the 
+goconvey-server to ensure that tests are being run and that the watcher 
+can be updated.
+"""
+
+
+import urllib2
+import json
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
