f20d03f8e822a1a3d0e227fce7f52825deaae038
Michael Whatcott <mdwhatcott@gmail.com>
2013-10-03 11:06:10 -0600

    Filled in some details for the integration test.

--- webserver/test_server.py
+++ webserver/test_server.py
@@ -10,8 +10,43 @@ import json
 
 
 def main():
+    # GET: /watcher: returns working directory
+    # POST: /watcher?new-dir: returns 200
+    # GET: /watcher: returns new-dir
+    
+    # create new package
+    # GET: /latest: returns latest output
+    # add new test file
+    # GET: /latest: returns latest output (including new test)
+    # remove test file
+    # GET: /latest: returns first output
+    # remove package
+    # GET: /latest: returns blank stuff?
     pass
 
 
 if __name__ == '__main__':
     main()
+
+
+TEST_CODE = """package testing
+
+import (
+    "testing"
+    . "github.com/smartystreets/goconvey/convey"
+)
+
+func TestSomething(t *testing.T) {
+    Convey("Something", t, func() {
+        Convey("should happen", func() {
+            So(true, ShouldBeTrue)
+        })
+    })
+}
+
+"""
+
+TESTS = {
+    'initial': ('initial_test.go', TEST_CODE),
+    'additional': ('additional_test.go', TEST_CODE.replace("Something", "Something2")),
+}
