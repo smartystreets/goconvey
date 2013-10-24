a9a26070cdd8a6dca9c801d72af406720389cccd
Michael Whatcott <mdwhatcott@gmail.com>
2013-10-24 17:10:06 -0600

    Initial functionality of scanner.

--- web/server/watcher/scanner.go
+++ web/server/watcher/scanner.go
@@ -1,20 +1,81 @@
 package watcher
 
 import (
-	"github.com/smartystreets/goconvey/web/server/system"
+	"github.com/smartystreets/goconvey/web/server/contract"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 type Scanner struct {
-	fs      system.FileSystem
-	watcher Watcher
-	// contains the FileSystem
-	// contains the Watcher
-	// NOTE: will have to signal creation and deletion of packages to the Watcher
+	fs      contract.FileSystem
+	watcher contract.Watcher
+	root    string
+	watched map[string]bool
+	ignored map[string]bool
+	current int64
+	working int64
 }
 
-func (self *Scanner) Scan(root string) (changed bool) { return false }
+// TODO: we must find out if a new directory has been added or if one has been removed!
 
-func NewScanner(fs system.FileSystem, watcher Watcher) *Scanner {
+func (self *Scanner) Scan(root string) (changed bool) {
+	self.gatherWatchedFolders()
+	self.fs.Walk(root, self.calculateWorkingChecksum)
+	return self.determineIfChanged()
+}
+
+func (self *Scanner) determineIfChanged() bool {
+	defer func() {
+		self.current = self.working
+		self.working = 0
+	}()
+
+	return self.working != self.current
+}
+func (self *Scanner) gatherWatchedFolders() {
+	self.watched = map[string]bool{}
+	self.ignored = map[string]bool{}
+
+	for i, path := range self.watcher.WatchedFolders() {
+		if i == 0 {
+			self.root = path.Path
+		}
+		if path.Active {
+			self.watched[path.Path] = true
+		} else {
+			self.ignored[path.Path] = true
+		}
+	}
+}
+
+func (self *Scanner) calculateWorkingChecksum(path string, info os.FileInfo, err error) error {
+	folder := deriveFolderName(path, info)
+
+	if self.ignored[folder] {
+		return nil
+	}
+
+	if !self.watched[folder] && strings.HasPrefix(path, self.root) && info.IsDir() {
+		self.working += info.Size() + info.ModTime().Unix()
+		self.watcher.Creation(folder)
+	} else if self.watched[folder] && filepath.Ext(path) == ".go" {
+		self.working += info.Size() + info.ModTime().Unix()
+	}
+
+	return nil
+}
+func deriveFolderName(path string, info os.FileInfo) string {
+	if info.IsDir() {
+		return path
+	} else {
+		return filepath.Dir(path)
+	}
+}
+
+func NewScanner(fs contract.FileSystem, watcher contract.Watcher) *Scanner {
 	self := &Scanner{}
+	self.fs = fs
+	self.watcher = watcher
 	return self
 }
--- web/server/watcher/scanner_test.go
+++ web/server/watcher/scanner_test.go
@@ -2,33 +2,150 @@ package watcher
 
 import (
 	. "github.com/smartystreets/goconvey/convey"
+	"github.com/smartystreets/goconvey/web/server/system"
 	"testing"
+	"time"
 )
 
 func TestScanner(t *testing.T) {
-	Convey("Subject: Scanner", t, func() {
-		Convey("As a result of calling Scan()", func() {
-			Convey("For the first time", func() {
+	var fixture *scannerFixture
+	var changed bool
+
+	Convey("To begin with, the scanner is provided a contrived file system environment", t, func() {
+		fixture = newScannerFixture()
+
+		Convey("When we call Scan() for the first time", func() {
+			changed = fixture.scan()
+
+			Convey("The scanner should report a change in state", func() {
+				So(changed, ShouldBeTrue)
+			})
+		})
+
+		Convey("Then, on subsequent calls to Scan()", func() {
+			changed = fixture.scan()
+
+			Convey("When the file system has not changed in any way", func() {
+
+				Convey("The scanner should NOT report any change in state", func() {
+					So(fixture.scan(), ShouldBeFalse)
+				})
+			})
+
+			Convey("When a new go file is created within a watched folder", func() {
 				Convey("The scanner should report a change in state", nil)
 			})
 
-			Convey("On subsequent calls", func() {
-				Convey("When the file system has not changed", func() {
-					Convey("The scanner should NOT report any change in state", nil)
-				})
+			Convey("When an existing go file within a watched folder has been modified", func() {
+				fixture.fs.Modify("/root/sub/file.go")
 
-				Convey("When an existing file on the file system has been modified", func() {
-					Convey("The scanner should report the change via its return value", nil)
+				Convey("The scanner should report a change in state", func() {
+					So(fixture.scan(), ShouldBeTrue)
 				})
+			})
 
-				Convey("When the file system has received a new directory", func() {
-					Convey("The scanner should report the addition to the watcher", nil)
-				})
+			Convey("When an existing go file within a watched folder has been renamed", func() {
+				fixture.fs.Rename("/root/sub/file.go", "/root/sub/asdf.go")
 
-				Convey("When the file system loses a directory", func() {
-					Convey("The scanner should report the deletion to the watcher", nil)
+				Convey("The scanner should report a change in state", func() {
+					So(fixture.scan(), ShouldBeTrue)
 				})
+			})
+
+			Convey("When an existing go file within a watched folder has been deleted", func() {
+				fixture.fs.Delete("/root/sub/file.go")
+
+				Convey("The scanner should report a change in state", func() {
+					So(fixture.scan(), ShouldBeTrue)
+				})
+			})
+
+			Convey("When a go file is created outside any watched folders", func() {
+				Convey("The scanner should NOT report a change in state", nil)
+			})
+
+			Convey("When a go file is modified outside any watched folders", func() {
+				Convey("The scanner should NOT report a change in state", nil)
+			})
+
+			Convey("When a go file is renamed outside any watched folders", func() {
+				Convey("The scanner should NOT report a change in state", nil)
+			})
+
+			Convey("When a go file is deleted outside any watched folders", func() {
+				Convey("The scanner should NOT report a change in state", nil)
+			})
+
+			Convey("When a miscellaneous file is created", func() {
+				Convey("The scanner should NOT report a change in state", nil)
+			})
+
+			Convey("When a miscellaneous file is modified", func() {
+				Convey("The scanner should NOT report a change in state", nil)
+			})
+
+			Convey("When a miscellaneous file is renamed", func() {
+				Convey("The scanner should NOT report a change in state", nil)
+			})
+
+			Convey("When a miscellaneous file is deleted", func() {
+				Convey("The scanner should NOT report a change in state", nil)
+			})
+
+			Convey("When a new folder is created inside a watched folder", func() {
+				Convey("The scanner should report the change", nil)
+				Convey("The scanner should notify the watcher of the creation", nil)
+			})
+
+			Convey("When a watched folder is deleted", func() {
+				Convey("The scanner should report the change", nil)
+				Convey("The scanner should notify the watcher of the deletion", nil)
+			})
+
+			Convey("When a folder is created outside any watched folders", func() {
+				Convey("The scanner should NOT report the change", nil)
+				Convey("The scanner should NOT notify the watcher of the change", nil)
+			})
+
+			Convey("When a folder that is not being watched is deleted", func() {
+				Convey("The scanner should NOT report the change", nil)
+				Convey("The scanner should NOT notify the watcher of the change", nil)
 			})
 		})
 	})
+}
+
+type scannerFixture struct {
+	scanner *Scanner
+	fs      *system.FakeFileSystem
+	watcher *Watcher
+}
+
+func (self *scannerFixture) scan() bool {
+	return self.scanner.Scan("/root")
+}
+func (self *scannerFixture) wasDeleted(folder string) bool {
+	return !self.wasCreated(folder)
+}
+func (self *scannerFixture) wasCreated(folder string) bool {
+	for _, w := range self.watcher.WatchedFolders() {
+		if w.Path == folder {
+			return true
+		}
+	}
+	return false
+}
+
+func newScannerFixture() *scannerFixture {
+	self := &scannerFixture{}
+	self.fs = system.NewFakeFileSystem()
+	self.fs.Create("/root", 0, time.Now())
+	self.fs.Create("/root/file.go", 1, time.Now())
+	self.fs.Create("/root/sub", 0, time.Now())
+	self.fs.Create("/root/sub/file.go", 2, time.Now())
+	self.fs.Create("/root/sub/empty", 0, time.Now())
+	self.watcher = NewWatcher(self.fs)
+	self.watcher.Adjust("/root")
+	self.scanner = NewScanner(self.fs, self.watcher)
+	return self
 }
