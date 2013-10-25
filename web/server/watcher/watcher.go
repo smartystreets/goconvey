84aa5acfd0a3e4da05a72b15efde88ea06f75250
Michael Whatcott <mdwhatcott@gmail.com>
2013-10-24 21:16:49 -0600

    Rename.

--- web/server/watcher/watcher.go
+++ web/server/watcher/watcher.go
@@ -9,8 +9,8 @@ import (
 )
 
 type Watcher struct {
-	fs contract.FileSystem
-	w  map[string]*contract.Package
+	fs      contract.FileSystem
+	watched map[string]*contract.Package
 }
 
 func (self *Watcher) Adjust(root string) error {
@@ -18,41 +18,41 @@ func (self *Watcher) Adjust(root string) error {
 		return errors.New(fmt.Sprintf("Directory does not exist: '%s'", root))
 	}
 
-	self.w = make(map[string]*contract.Package)
+	self.watched = make(map[string]*contract.Package)
 	self.fs.Walk(root, self.includeFolders)
 
 	return nil
 }
 func (self *Watcher) includeFolders(path string, info os.FileInfo, err error) error {
 	if info.IsDir() {
-		self.w[path] = &contract.Package{Active: true, Path: path, Name: info.Name()}
+		self.watched[path] = &contract.Package{Active: true, Path: path, Name: info.Name()}
 	}
 	return nil
 }
 
 func (self *Watcher) Deletion(folder string) {
-	delete(self.w, folder)
+	delete(self.watched, folder)
 }
 
 func (self *Watcher) Creation(folder string) {
-	self.w[folder] = &contract.Package{Active: true, Path: folder, Name: filepath.Base(folder)}
+	self.watched[folder] = &contract.Package{Active: true, Path: folder, Name: filepath.Base(folder)}
 }
 
 func (self *Watcher) Ignore(folder string) error {
-	if value, exists := self.w[folder]; exists {
+	if value, exists := self.watched[folder]; exists {
 		value.Active = false
 	}
 	return nil
 }
 func (self *Watcher) Reinstate(folder string) error {
-	if value, exists := self.w[folder]; exists {
+	if value, exists := self.watched[folder]; exists {
 		value.Active = true
 	}
 	return nil
 }
 func (self *Watcher) WatchedFolders() []*contract.Package {
-	i, watched := 0, make([]*contract.Package, len(self.w))
-	for _, item := range self.w {
+	i, watched := 0, make([]*contract.Package, len(self.watched))
+	for _, item := range self.watched {
 		watched[i] = item
 		i++
 	}
@@ -60,27 +60,27 @@ func (self *Watcher) WatchedFolders() []*contract.Package {
 }
 
 func (self *Watcher) IsActive(folder string) bool {
-	if value, exists := self.w[folder]; exists {
+	if value, exists := self.watched[folder]; exists {
 		return value.Active
 	}
 	return false
 }
 
 func (self *Watcher) IsIgnored(folder string) bool {
-	if value, exists := self.w[folder]; exists {
+	if value, exists := self.watched[folder]; exists {
 		return !value.Active
 	}
 	return false
 }
 
 func (self *Watcher) IsWatched(folder string) bool {
-	_, exists := self.w[folder]
+	_, exists := self.watched[folder]
 	return exists
 }
 
 func NewWatcher(fs contract.FileSystem) *Watcher {
 	self := &Watcher{}
 	self.fs = fs
-	self.w = map[string]*contract.Package{}
+	self.watched = map[string]*contract.Package{}
 	return self
 }
