4c3cb5a70f158bede489398eba4b6c87d06056de
Michael Whatcott <mdwhatcott@gmail.com>
2013-10-31 22:30:06 -0600

    Much cleaner.
    Note the clever usage of the fixture methods filling in as the func()
    for the assertion Convey calls.

--- web/server/executor/tester_test.go
+++ web/server/executor/tester_test.go
@@ -1,62 +1,40 @@
 package executor
 
 import (
+	"fmt"
 	. "github.com/smartystreets/goconvey/convey"
 	"strings"
 	"testing"
 	"time"
 )
 
-func TestTester(t *testing.T) {
-	var (
-		tester       *ConcurrentTester
-		shell        *TimedShell
-		results      []string
-		compilations []*ShellCommand
-		executions   []*ShellCommand
-	)
+func TestConcurrentTester(t *testing.T) {
+	var fixture *TesterFixture
 
-	Convey("Subject: Test controlled execution of tests", t, func() {
-		shell = NewTimedShell()
-		tester = NewConcurrentTester(shell)
-		folders := []string{"a", "b", "c"}
+	Convey("Subject: Controlled (and concurrent) execution of test packages", t, func() {
+		fixture = NewTesterFixture()
 
-		Convey("When packages are executed synchronously", func() {
-			results = tester.TestAll(folders)
-			compilations = shell.Compilations()
-			executions = shell.Executions()
+		Convey("When tests for each package are executed", func() {
+			fixture.RunTests()
 
-			Convey("The tester should build all dependencies of input folders", func() {
-				for i, input := range folders {
-					So(compilations[i].Command, ShouldEqual, "go test -i "+input)
-				}
-			})
+			Convey("The tester should build all dependencies of input packages",
+				fixture.ShouldHaveRecordOfCompilationCommands)
 
-			Convey("The tester should execute the tests in each folder with the correct arguments", func() {
-				for i, input := range folders {
-					So(executions[i].Command, ShouldEqual, "go test -v -timeout=-42s "+input)
-				}
-			})
+			Convey("The tester should execute the tests in each package with the correct arguments",
+				fixture.ShouldHaveRecordOfExecutionCommands)
 
-			Convey("Each package should be run after the other in the given order", func() {
-				for i := 0; i < len(executions)-1; i++ {
-					current := executions[i]
-					next := executions[i+1]
-					So(current.Started, ShouldHappenBefore, next.Started)
-					So(current.Ended, ShouldHappenOnOrBefore, next.Started)
-				}
-			})
+			Convey("There should be a test output result for each package",
+				fixture.ShouldHaveOneOutputPerInput)
 
-			Convey("There should be a test output result for each input folder", func() {
-				So(len(results), ShouldEqual, len(folders))
+			Convey("The output should be as expected",
+				fixture.OutputShouldBeAsExpected)
+		})
 
-			})
+		Convey("When the tests for each package are executed synchronously", func() {
+			fixture.RunTests()
 
-			Convey("The output should be as expected", func() {
-				for i, _ := range folders {
-					So(results[i], ShouldEqual, executions[i].Command)
-				}
-			})
+			Convey("Each package should be run synchronously and in the given order",
+				fixture.CheckContiguousExecution)
 		})
 
 		Convey("When packages are tested in batches", func() {
@@ -64,6 +42,63 @@ func TestTester(t *testing.T) {
 		})
 	})
 }
+
+type TesterFixture struct {
+	tester       *ConcurrentTester
+	shell        *TimedShell
+	results      []string
+	compilations []*ShellCommand
+	executions   []*ShellCommand
+	packages     []string
+}
+
+func NewTesterFixture() *TesterFixture {
+	self := &TesterFixture{}
+	self.shell = NewTimedShell()
+	self.tester = NewConcurrentTester(self.shell)
+	self.packages = []string{"a", "b", "c", "d"}
+	return self
+}
+
+func (self *TesterFixture) RunTests() {
+	self.results = self.tester.TestAll(self.packages)
+	self.compilations = self.shell.Compilations()
+	self.executions = self.shell.Executions()
+}
+
+func (self *TesterFixture) ShouldHaveRecordOfCompilationCommands() {
+	for i, pkg := range self.packages {
+		command := self.compilations[i].Command
+		So(command, ShouldEqual, "go test -i "+pkg)
+	}
+}
+
+func (self *TesterFixture) ShouldHaveRecordOfExecutionCommands() {
+	for i, pkg := range self.packages {
+		So(self.executions[i].Command, ShouldEqual, "go test -v -timeout=-42s "+pkg)
+	}
+}
+
+func (self *TesterFixture) ShouldHaveOneOutputPerInput() {
+	So(len(self.results), ShouldEqual, len(self.packages))
+}
+
+func (self *TesterFixture) OutputShouldBeAsExpected() {
+	for i, _ := range self.packages {
+		So(self.results[i], ShouldEqual, self.executions[i].Command)
+	}
+}
+
+func (self *TesterFixture) CheckContiguousExecution() {
+	for i := 0; i < len(self.executions)-1; i++ {
+		current := self.executions[i]
+		next := self.executions[i+1]
+		So(current.Started, ShouldHappenBefore, next.Started)
+		So(current.Ended, ShouldHappenOnOrBefore, next.Started)
+	}
+}
+
+/**** Fakes ****/
 
 type ShellCommand struct {
 	Command string
@@ -85,21 +120,21 @@ func (self *TimedShell) Executions() []*ShellCommand {
 }
 
 func (self *TimedShell) Execute(name string, args ...string) (output string, err error) {
-	output = name + " " + strings.Join(args, " ")
-	start := time.Now()
-	nap, err := time.ParseDuration("10ms")
-	if err != nil {
-		panic(err)
-	}
-	time.Sleep(nap)
-	end := time.Now()
-	command := &ShellCommand{output, start, end}
-	if strings.Contains(output, " -i ") {
+	command := self.composeCommand(name + " " + strings.Join(args, " "))
+	output = command.Command
+
+	if strings.Contains(command.Command, " -i ") {
 		self.compilations = append(self.compilations, command)
 	} else {
 		self.executions = append(self.executions, command)
 	}
 	return
+}
+func (self *TimedShell) composeCommand(commandText string) *ShellCommand {
+	start := time.Now()
+	time.Sleep(nap)
+	end := time.Now()
+	return &ShellCommand{commandText, start, end}
 }
 func (self *TimedShell) Getenv(key string) string {
 	panic("NOT SUPPORTED")
@@ -114,3 +149,6 @@ func NewTimedShell() *TimedShell {
 	self.compilations = []*ShellCommand{}
 	return self
 }
+
+var nap, _ = time.ParseDuration("10ms")
+var _ = fmt.Sprintf("fmt")
