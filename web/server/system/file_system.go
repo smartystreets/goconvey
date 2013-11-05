e2a3a8ea49b577f6630441055e7ad2092c034688
Michael Whatcott <mdwhatcott@gmail.com>
2013-11-05 16:57:05 -0700

    Ignoring .git paths.

--- web/server/system/file_system.go
+++ web/server/system/file_system.go
@@ -3,17 +3,29 @@ package system
 import (
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type FileSystem struct{}
 
 func (self *FileSystem) Walk(root string, step filepath.WalkFunc) {
-	err := filepath.Walk(root, step)
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if self.shouldIgnore(path) {
+			return nil
+		}
+
+		return step(path, info, err)
+	})
 
 	if err != nil {
 		panic(err) // TODO?
 	}
 }
+
+func (self *FileSystem) shouldIgnore(path string) bool {
+	return strings.Contains(path, string(filepath.Separator)+".git")
+}
+
 func (self *FileSystem) Exists(directory string) bool {
 	info, err := os.Stat(directory)
 	return err == nil && info.IsDir()
