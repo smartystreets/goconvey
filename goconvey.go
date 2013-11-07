7fc880844f3c76c8e76d9239468dd0e5c3d2dd69
Michael Whatcott <mdwhatcott@gmail.com>
2013-11-07 09:57:49 -0700

    Cleanup and updated README with new server executable name. Default port: 8080.

--- goconvey.go
+++ goconvey.go
@@ -29,7 +29,7 @@ import (
 
 func init() {
 	quarterSecond, _ := time.ParseDuration("250ms")
-	flag.IntVar(&port, "port", 8081, "The port at which to serve http.")
+	flag.IntVar(&port, "port", 8080, "The port at which to serve http.")
 	flag.StringVar(&host, "host", "127.0.0.1", "The host at which to serve http.")
 	flag.DurationVar(&nap, "poll", quarterSecond, "The interval to wait between polling the file system for changes (default: 250ms).")
 	flag.IntVar(&packages, "packages", 10, "The number of packages to test in parallel. Higher == faster but more costly in terms of computing. (default: 10)")
