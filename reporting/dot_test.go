9d78ff5aee5867e09fa8361db2c40e31230bc488
Michael Whatcott <mdwhatcott@gmail.com>
2014-02-18 10:25:57 -0700

    Dot reporting is covered.

--- reporting/dot_test.go
+++ reporting/dot_test.go
@@ -22,3 +22,19 @@ func TestDotReporterAssertionPrinting(t *testing.T) {
 		t.Errorf("\nExpected: '%s'\nActual:  '%s'", expected, file.buffer)
 	}
 }
+
+func TestDotReporterOnlyReportsAssertions(t *testing.T) {
+	monochrome()
+	file := newMemoryFile()
+	printer := NewPrinter(file)
+	reporter := NewDotReporter(printer)
+
+	reporter.BeginStory(NewStoryReport(t))
+	reporter.Enter(NewScopeReport("title", "name"))
+	reporter.Exit()
+	reporter.EndStory()
+
+	if file.buffer != "" {
+		t.Errorf("\nExpected: '(blank)'\nActual:  '%s'", file.buffer)
+	}
+}
