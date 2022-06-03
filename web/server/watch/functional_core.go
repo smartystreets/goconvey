c50fdf16c95e30198f2f94fea0e4278fc56b196b
Lucas Lee <lihui.lucas@apemans.com>
2022-06-03 11:33:39 +0800

    added main.goconvey

--- web/server/watch/functional_core.go
+++ web/server/watch/functional_core.go
@@ -2,6 +2,7 @@ package watch
 
 import (
 	"os"
+	"path"
 	"path/filepath"
 	"strings"
 
@@ -114,9 +115,20 @@ func LimitDepth(folders messaging.Folders, depth int) {
 ///////////////////////////////////////////////////////////////////////////////
 
 func AttachProfiles(folders messaging.Folders, items []*FileSystemItem) {
+	var rootProfile *FileSystemItem
+	for _, profile := range items {
+		path, _ := path.Split(profile.Path)
+		if path == profile.Root && profile.Name == "main.goconvey" {
+			rootProfile = profile
+			break
+		}
+	}
+
 	for _, profile := range items {
 		if folder, exists := folders[filepath.Dir(profile.Path)]; exists {
 			folder.Disabled, folder.BuildTags, folder.TestArguments = profile.ProfileDisabled, profile.ProfileTags, profile.ProfileArguments
+		} else if rootProfile != nil {
+			folder.Disabled, folder.BuildTags, folder.TestArguments = rootProfile.ProfileDisabled, rootProfile.ProfileTags, rootProfile.ProfileArguments
 		}
 	}
 }
